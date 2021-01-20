0e0cccf27b2202666475a7ed6e4b79a2daa89b82
testing-ddl <68757563+testing-ddl@users.noreply.github.com>
2021-01-19 18:04:01 -0800

    Create job_write.py

---
+++ job_write.py
@@ -0,0 +1,4 @@
+with open("/mnt/code/job_file.txt", "w+") as code_file:
+    code_file.write("some text here")
+with open("/mnt/artifacts/job_file.txt", "w+") as artifact_file:
+    artifact_file.write("some text here")   
