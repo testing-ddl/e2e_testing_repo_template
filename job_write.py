fc4b39d9e9e4df7572ed4bfe044092f844289052
testing-ddl <68757563+testing-ddl@users.noreply.github.com>
2021-01-19 18:07:30 -0800

    Update job_write.py

--- job_write.py
+++ job_write.py
@@ -1,4 +1,4 @@
 with open("/mnt/code/job_file.txt", "w+") as code_file:
-    code_file.write("some text here")
+    code_file.write("file in code")
 with open("/mnt/artifacts/job_file.txt", "w+") as artifact_file:
-    artifact_file.write("some text here")   
+    artifact_file.write("file in artifacts")   
