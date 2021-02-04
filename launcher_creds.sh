2e0dd196248cd073a9c17017184f1f9ac7aa5f13
ddl-kyuvzhik <65196128+ddl-kyuvzhik@users.noreply.github.com>
2021-02-03 16:16:13 -0800

    Update launcher_creds.sh

--- launcher_creds.sh
+++ launcher_creds.sh
@@ -3,7 +3,7 @@ ssh-keyscan github.com  >> ~/.ssh/known_hosts
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 echo "TIME STAMP" $DATE_WITH_TIME
 pwd
-echo $DATE_WITH_TIME >> launcher.txt
+echo $DATE_WITH_TIME >> launcher_repo.txt
 git add .
 git commit -m "test launcher user"
 git push
