82b0367860fa8c092b37dd5329d104bd4c6e6972
testing-ddl <68757563+testing-ddl@users.noreply.github.com>
2021-08-06 18:25:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from pip._internal.utils.typing import MYPY_CHECK_RUNNING
+try:
+    from pip._internal.utils.typing import MYPY_CHECK_RUNNING
+except ModuleNotFoundError:
+    MYPY_CHECK_RUNNING = False
 
 if MYPY_CHECK_RUNNING:
     from typing import List, Optional
@@ -8,7 +11,6 @@ def main(args=None):
     # type: (Optional[List[str]]) -> int
     """This is preserved for old console scripts that may still be referencing
     it.
-
     For additional details, see https://github.com/pypa/pip/issues/7498.
     """
     from pip._internal.utils.entrypoints import _wrapper
