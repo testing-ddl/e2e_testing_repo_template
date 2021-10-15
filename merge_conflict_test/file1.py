c3adb6db2fd140215f53ddb8dca73e9f58ebf9fb
Rfqa 009 GBP <rfqa+009-gbp@dominodatalab.com>
2021-10-15 16:40:47 -0700

    Pushed from Domino: https://mergconf5227.quality-team-sandbox.domino.tech/workspace/rfqa-009-gbp/github_cloud_new_pat_ssh_default?executionId=6169f654284c0c2a1b36dedf

--- merge_conflict_test/file1.py
+++ merge_conflict_test/file1.py
@@ -1,5 +1,1 @@
-<<<<<<< Upstream, based on origin/main
-this will be pushed to remote
-=======
-this will be a conflicting change in code
->>>>>>> ecc4036 
+this will be pushed to remote to become a conflict
